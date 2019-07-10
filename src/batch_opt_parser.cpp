0e024e8e77bcc768fec5dc1f4e7caac87816a879
TechanteNG <technate@protonmail.com>
2019-07-10 19:31:08 +0200

    Tests for batch_opt_parser

--- src/batch_opt_parser.cpp
+++ src/batch_opt_parser.cpp
@@ -3,7 +3,10 @@
 std::vector<std::string> dbust::models::BatchOptParser::parse(const char* opt) const
 {
     std::string_view r_opt(opt);
-    auto f_opt = r_opt.substr(1, r_opt.length() - 2);
+    auto f_opt =
+            (r_opt[0] == '\'' && r_opt[r_opt.size() -1 ] == '\'') ||
+            (r_opt[0] == '"' && r_opt[r_opt.size() - 1] == '\"') ?
+            r_opt.substr(1, r_opt.length() - 2) : r_opt;
     std::vector<std::string> res;
     std::string tmp;
     for (const char c : f_opt)
